a6607b100239e23695b37e980088e6383def8da6
Roman Mazur <roman.mazur@hotmail.com>
2024-01-25 14:11:33 +0100

    Change default cairo code

--- components/Editor/examples.ts
+++ components/Editor/examples.ts
@@ -2,8 +2,19 @@ import { ExampleCode } from './types'
 
 const examples: ExampleCode = {
   Cairo: [
-    `fn main() -> felt252 {
-    1
+`use core::felt252;
+
+fn main() -> felt252 {
+    let n = 10;
+    let result = fib(1, 1, n);
+    result
+}
+
+fn fib(a: felt252, b: felt252, n: felt252) -> felt252 {
+    match n {
+        0 => a,
+        _ => fib(b, a + b, n - 1),
+    }
 }`,
   ],
   Sierra: [
