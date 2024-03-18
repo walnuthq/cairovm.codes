8b14a86e59acaa1f51ffa68e677f85bf5a9ab459
ArnaudBD <arnaud.bijuduval@gmail.com>
2024-03-18 13:57:58 +0100

    fix: Fix thead overlay issue on scroll

--- components/Tracer/index.tsx
+++ components/Tracer/index.tsx
@@ -330,8 +330,8 @@ function InstructionsTable({
 
   return (
     <table className="w-full font-mono text-tiny">
-      <thead>
-        <tr className="text-left sticky top-0 bg-gray-50 dark:bg-black-600 text-gray-400 dark:text-gray-600 border-b border-gray-200 dark:border-black-500">
+      <thead style={{ zIndex: 2, position: 'sticky', top: 0 }}>
+        <tr className="text-left bg-gray-50 dark:bg-black-600 text-gray-400 dark:text-gray-600 border-b border-gray-200 dark:border-black-500">
           <th className="py-1"></th>
           <th className="py-1"></th>
           <th className="py-1"></th>
