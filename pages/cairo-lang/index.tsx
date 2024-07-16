7a69ac7143d456e390b79a57ea1a735affb1816f
melnikga <melnik.goshan2013@gmail.com>
2024-07-16 16:49:41 +0300

    Updated metadata

--- pages/cairo-lang/index.tsx
+++ pages/cairo-lang/index.tsx
@@ -20,10 +20,10 @@ const CairoLangPage = () => {
     <>
       <Head>
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="EVM Codes - Opcodes" />
+        <meta property="og:title" content="Cairo VM Codes" />
         <meta
           name="description"
-          content="An Ethereum Virtual Machine Opcodes Interactive Reference"
+          content="A Cairo Virtual Machine Interactive Reference"
         />
       </Head>
       <Editor isCairoLangPage={true} />
