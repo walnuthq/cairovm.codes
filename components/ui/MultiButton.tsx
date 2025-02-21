b8b192a6620929916d761414fa3935a85438d76c
Melnik George <91109124+melnikga@users.noreply.github.com>
2025-02-21 16:49:52 +0300

    Added hover (#205)

--- components/ui/MultiButton.tsx
+++ components/ui/MultiButton.tsx
@@ -49,13 +49,12 @@ const MultiButton = ({ onCompileRun }: MultiButtonProps) => {
           <ChevronDownIcon aria-hidden="true" className="w-5" color="black" />
           <span className="absolute left-0 top-1/2 h-2/3 w-[1px] bg-[#00000033] -translate-y-1/2" />
         </MenuButton>
-
         <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded shadow-lg focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in dark:bg-darkMode-primary bg-white">
-          <div className="py-1">
+          <div>
             <MenuItem>
               <button
                 onClick={() => setSelected('run')}
-                className={`block w-full px-4 py-2 text-sm text-left dark:text-darkMode-text text-gray-900`}
+                className="block w-full px-4 pt-3 pb-2 text-sm text-left dark:text-darkMode-text text-gray-900 hover:bg-gray-100 dark:hover:bg-darkMode-secondary hover:rounded-t rounded-none"
               >
                 Run
               </button>
@@ -63,7 +62,7 @@ const MultiButton = ({ onCompileRun }: MultiButtonProps) => {
             <MenuItem>
               <button
                 onClick={() => setSelected('run-prove')}
-                className={`block w-full px-4 py-2 text-sm text-left dark:text-darkMode-text text-gray-900`}
+                className="block w-full px-4 py-2 text-sm text-left dark:text-darkMode-text text-gray-900 hover:bg-gray-100 dark:hover:bg-darkMode-secondary rounded-none"
               >
                 Run and prove
               </button>
@@ -71,7 +70,7 @@ const MultiButton = ({ onCompileRun }: MultiButtonProps) => {
             <MenuItem>
               <button
                 onClick={() => setSelected('run-prove-verify')}
-                className={`block w-full px-4 py-2 text-sm text-left dark:text-darkMode-text text-gray-900`}
+                className="block w-full px-4 pt-2 pb-3 text-sm text-left dark:text-darkMode-text text-gray-900 hover:bg-gray-100 dark:hover:bg-darkMode-secondary hover:rounded-b rounded-none"
               >
                 Run, prove and verify
               </button>
