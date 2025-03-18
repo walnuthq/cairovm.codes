571d1c0f1395adb93b01ca260ebc3d37c2a83184
Melnik George <melnik.goshan2013@gmail.com>
2025-03-18 13:01:51 +0300

    Update ExampleSelector.tsx

--- components/Editor/ExampleSelector.tsx
+++ components/Editor/ExampleSelector.tsx
@@ -1,15 +1,6 @@
 import { useId } from 'react'
 
-import { RiFileCodeLine } from '@remixicon/react'
-import Select, {
-  components,
-  DropdownIndicatorProps,
-  GroupBase,
-  StylesConfig,
-} from 'react-select'
-
-import { cn } from '../../util/styles'
-import { Button } from '../ui'
+import Select from 'react-select'
 
 import { CairoExampleNames, Examples } from './examples'
 
@@ -32,12 +23,13 @@ function ExampleSelector({ onExampleChange }: Props) {
     <Select
       onChange={onExampleChange}
       options={examplesOptions}
-      components={{
-        DropdownIndicator,
-      }}
-      controlShouldRenderValue={false}
+      // components={{
+      //   DropdownIndicator,
+      // }}
+      // controlShouldRenderValue={false}
       classNamePrefix="select"
-      styles={reactSelectStyles}
+      // styles={reactSelectStyles}
+      placeholder={'Choose Cairo Example'}
       menuPlacement="top"
       isSearchable={false}
       instanceId={useId()}
@@ -47,41 +39,41 @@ function ExampleSelector({ onExampleChange }: Props) {
 
 export default ExampleSelector
 
-const reactSelectStyles: StylesConfig<
-  SelectOption,
-  false,
-  GroupBase<SelectOption>
-> = {
-  // as we dont want to show value field in react-select
-  // and just want to show an icon, but input field takes some space
-  // so we hide the valueContainer with css properties
-  // Question: Why not remove the ValueContainer completely from 'components' prop in react-select?
-  // Ans: react-select needs a valuefield
-  //      as many features like click outside to close drop down etc depends on it
-  //      so we just add display none (it doesn't hurt)
-  valueContainer: (styles) => ({
-    ...styles,
-    width: '0',
-    opacity: 0,
-    padding: '0 !important',
-  }),
-}
+// const reactSelectStyles: StylesConfig<
+//   SelectOption,
+//   false,
+//   GroupBase<SelectOption>
+// > = {
+//   // as we dont want to show value field in react-select
+//   // and just want to show an icon, but input field takes some space
+//   // so we hide the valueContainer with css properties
+//   // Question: Why not remove the ValueContainer completely from 'components' prop in react-select?
+//   // Ans: react-select needs a valuefield
+//   //      as many features like click outside to close drop down etc depends on it
+//   //      so we just add display none (it doesn't hurt)
+//   valueContainer: (styles) => ({
+//     ...styles,
+//     width: '0',
+//     opacity: 0,
+//     padding: '0 !important',
+//   }),
+// }
 
-const DropdownIndicator = (props: DropdownIndicatorProps<SelectOption>) => {
-  return (
-    <components.DropdownIndicator {...props}>
-      <Button
-        transparent
-        padded={false}
-        tooltip="Cairo Examples"
-        tooltipId="cairo-examples"
-        className={cn(
-          'p-2 text-[#E85733] dark:text-darkMode-icons hover:text-[#fc9278] focus:outline-none',
-          props.selectProps.menuIsOpen ? 'bg-black-900/5 dark:bg-white/5' : '',
-        )}
-      >
-        <RiFileCodeLine size={16} />
-      </Button>
-    </components.DropdownIndicator>
-  )
-}
+// const DropdownIndicator = (props: DropdownIndicatorProps<SelectOption>) => {
+//   return (
+//     <components.DropdownIndicator {...props}>
+//       <Button
+//         transparent
+//         padded={false}
+//         tooltip="Cairo Examples"
+//         tooltipId="cairo-examples"
+//         className={cn(
+//           'p-2 text-[#E85733] dark:text-darkMode-icons hover:text-[#fc9278] focus:outline-none',
+//           props.selectProps.menuIsOpen ? 'bg-black-900/5 dark:bg-white/5' : '',
+//         )}
+//       >
+//         <RiFileCodeLine size={16} />
+//       </Button>
+//     </components.DropdownIndicator>
+//   )
+// }
